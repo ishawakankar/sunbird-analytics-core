3bb4ad724abc0e5cdf9e065762a78735573e9fd0
Isha Wakankar <iwakankar@hotmail.com>
2020-07-07 22:59:45 +0530

    .

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/IjobMetrics.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/IjobMetrics.scala
@@ -3,5 +3,5 @@ package org.ekstep.analytics.framework
 import org.apache.spark.SparkContext
 
 trait IjobMetrics {
-  def main(config: List[String])(implicit sc: Option[SparkContext] = None, fc: Option[FrameworkContext] = None);
+  def main(config: String, batchId: String)(implicit sc: Option[SparkContext] = None, fc: Option[FrameworkContext] = None);
 }
